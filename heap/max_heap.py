117590166cf31b69e87d838085b794b78de7a16e
JTyzz <jtyzzer@gmail.com>
2019-07-24 14:15:35 -0700

    Half of max heap done

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,31 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+
+    #bubble_up checks if its parent is larger and swap if so
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
     pass
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    while index > 0:
+      parent = (index - 1) // 2
+
+      # is current element larger than parent
+      if self.storage[index] > self.storage[parent]:
+        #if so swap
+        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+        index = parent
+      else:
+        break
 
   def _sift_down(self, index):
     pass
