25ff69f1605bb76e25edc42f1b42767fb8e4447a
JTyzz <jtyzzer@gmail.com>
2019-07-24 10:01:03 -0700

    text buffer

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,46 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+      #lesser on left, greater on right
+   if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+   elif value >= self.value:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if self.value == target:
+        return True
+    if target < self.value :
+      if not self.left:
+        pass # do nothing because it is recursive loop
+      else:
+        if self.left.contains(target):
+          return True
+
+    elif target > self.value:
+      if not self.right:
+        pass
+      else:
+        if self.right.contains(target) :
+          return True
 
   def get_max(self):
-    pass
+    if self.right is None:
+      return self.value
+    else:
+      return self.right.get_max()
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+    
+    if self.left:
+      self.left.for_each(cb)
+    
+    if self.right:
+      self.right.for_each(cb)
